3c758d262c8a9438d7fc3dfc1db31f889771ad4d
DaTA677 <78419826+DaTA677@users.noreply.github.com>
2024-07-14 18:24:10 +0100

    Removed unnecessary comment lines

--- constProject/PracticeClass.cpp
+++ constProject/PracticeClass.cpp
@@ -7,14 +7,14 @@ PracticeClass::PracticeClass():num2(12)
 
 std::ostream& operator<<(std::ostream& out, const PracticeClass& obj)
 {
-	// TODO: insert return statement here
+	
 	out << "Num1: " << obj.num1 << " Num2: " << obj.num2;
 	return out;
 }
 
 std::istream& operator>>(std::istream& in, PracticeClass& obj)
 {
-	// TODO: insert return statement here
+	
 	std::cout << "Enter the num:";
 	in >> obj.num1;
 	return in;
